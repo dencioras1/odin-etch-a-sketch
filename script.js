daa52e72c25ca914101c2633e2fc0c3fb119db58
dencioras1 <denaskh@gmail.com>
2025-01-22 20:53:30 +0100

    Added a temporary input field and a button that takes in a value between 16 and 100 (no strings)

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const inputGrid = document.getElementById(`gridSize`);
 const gridButton = document.getElementById(`gridButton`);
 
-gridButton.addEventListener(`click`, () => {
-    let value = parseInt(inputGrid.value);
-    inputGrid.value = ``;
+let gridSize = 16;
 
-    console.log(value);
+gridButton.addEventListener(`click`, () => {
+    let value = 0;
+
+    if (!isNaN(parseInt(inputGrid.value))) {
+        if (parseInt(inputGrid.value) >= 16 && parseInt(inputGrid.value) <= 100) {
+            value = parseInt(inputGrid.value);
+            console.log(value);
+            inputGrid.value = ``;
+        }
+        else {
+            inputGrid.value = ``;
+            alert(`Error! Input must a number between 16 and 100`);
+        }
+    }
+    else {
+        inputGrid.value = ``;
+        alert(`Error! Input must a number between 16 and 100`);
+    }
+
+    
+    
 });
